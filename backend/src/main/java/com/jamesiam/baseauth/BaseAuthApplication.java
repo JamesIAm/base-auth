8628828325d82c1a172f39415f3db22425049726
James Nahajski <james.nahajski+github@gmail.com>
2024-06-21 21:17:20 +0100

    Typo in app name

--- backend/src/main/java/com/jamesiam/baseauth/BaseAuthApplication.java
+++ backend/src/main/java/com/jamesiam/baseauth/BaseAuthApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseauthApplication {
+public class BaseAuthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseauthApplication.class, args);
+		SpringApplication.run(BaseAuthApplication.class, args);
 	}
 
 }
