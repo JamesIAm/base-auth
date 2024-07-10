ab6a6031bbd8450c89c99bdc442207e9038c876d
James Nahajski <james.nahajski@gmail.com>
2024-07-10 22:52:32 +0100

    adding backend controllers that are protected and unprotected
    The unprotected controller can be accessed without any sort of authentication
    The proteceted controller cannot

---
+++ backend/src/main/java/com/jamesiam/baseauth/controller/ProtectedController.java
@@ -0,0 +1,12 @@
+package com.jamesiam.baseauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProtectedController {
+    @GetMapping("/protected")
+    private String sayHello() {
+        return "Hello from a protected endpoint";
+    }
+}
---
+++ backend/src/main/java/com/jamesiam/baseauth/controller/UnprotectedController.java
@@ -0,0 +1,12 @@
+package com.jamesiam.baseauth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UnprotectedController {
+    @GetMapping("/unprotected")
+    private String sayHello() {
+        return "Hello from an unprotected endpoint";
+    }
+}
