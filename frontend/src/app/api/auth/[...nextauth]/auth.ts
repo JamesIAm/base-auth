f6d3cc9b0f3d9d0f8af3b59d563b367373c0c4ab
James Nahajski <james.nahajski@gmail.com>
2024-07-10 22:54:34 +0100

    remove google as a provider
    As it doesn't seem to offer easy access to it's resource server to verify

--- frontend/src/app/api/auth/[...nextauth]/auth.ts
+++ frontend/src/app/api/auth/[...nextauth]/auth.ts
@@ -6,7 +6,6 @@ import type {
 import type { NextAuthOptions } from "next-auth";
 import { getServerSession } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 
 // You'll need to import and pass this
 // to `NextAuth` in `app/api/auth/[...nextauth]/route.ts`
@@ -16,10 +15,6 @@ export const authOptions: NextAuthOptions = {
 		GithubProvider({
 			clientId: process.env.GITHUB_ID as string,
 			clientSecret: process.env.GITHUB_SECRET as string,
-		}),
-		Google({
-			clientId: process.env.GOOGLE_CLIENT_ID as string,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 		// ...add more providers here
 	],
